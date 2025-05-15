50f7a255a9ed858f266356f24b573340846d7396
giprovazi <giovannirprovazi@gmail.com>
2025-05-15 11:32:42 -0300

    Ajuste de responsividade

--- frontend/src/components/BarraPesquisa.jsx
+++ frontend/src/components/BarraPesquisa.jsx
@@ -2,11 +2,11 @@ import { Search } from 'lucide-react';
 
 const BarraPesquisa = () => {
   return (
-    <div className="flex items-center bg-gray-200 rounded-md px-3 py-1 mt-4 w-80 md:w-96 lg:w-[57rem] h-[4rem]">
+    <div className="flex items-center bg-gray-200 rounded-md px-3 py-1 mt-4 w-80 md:w-[39rem] h-[4rem] lg:w-[48rem] h-[4rem] xl:w-[57rem] h-[4rem]">
       <input
         type="text"
         placeholder="Busque por jogos..."
-        className="bg-transparent w-full focus:outline-none placeholder-gray-500 placeholder:text-lg text-gray-800"
+        className="bg-transparent w-full focus:outline-none placeholder-gray-500 placeholder: md:text-2xl lg:text-2xl  xl:text-2xl text-gray-800"
       />
       <button className="ml-2 text-gray-600 hover:text-gray-800">
         <Search size={20} />
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,15 +5,17 @@ import { ShoppingBag, ShoppingCart } from 'lucide-react';
 const Header = () => {
     return (
     <header className='flex '>
-      <img src='./src/assets/logo.png' alt='Logo da empresa' className='w-[10rem] md:w-[12rem] lg:w-[13rem] xl:[15rem] object-contain p-4'/>
+      <img src='./src/assets/logo.png' alt='Logo da empresa' className='w-[10rem] md:w-[11rem] lg:w-[12rem] xl:[15rem] object-contain p-4'/>
       <nav className='flex flex-col'>
-        <ul className='flex space-x-2 md:space-x-16 lg:space-x-16 text-2xl md:text-5xl lg:text-76xl xl:text-7xl'>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/produtos">Produtos</Link></li>
-          <li><Link to="/sobre">Sobre</Link></li>
-          <li><Link to="/contato">Contato</Link></li>
-          <ShoppingCart size={64} className="text-white mt-3"/>
-        </ul>
+        <div className='flex gap-16'>
+          <ul className='flex space-x-2 md:space-x-12 lg:space-x-16 xl:space-x-16 text-2xl md:text-5xl lg:text-6xl xl:text-7xl'>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/produtos">Produtos</Link></li>
+            <li><Link to="/sobre">Sobre</Link></li>
+            <li><Link to="/contato">Contato</Link></li>
+          </ul>
+          <ShoppingCart size={80} className="text-white mt-3  w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-50 xl:h-50"/>
+        </div>
         <BarraPesquisa />
       </nav>
     </header>
