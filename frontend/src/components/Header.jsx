410c026662b90f04fa41686d8c51104d90c157a4
Giovanni <giovannirprovazi@gmail.com>
2025-05-16 19:49:45 -0300

    Continuando criacao da pagina home

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
     return (
-   <header className='flex items-center justify-between p-4 '>
-      <div className="flex items-center gap-[6rem]">
+   <header>
+      <div className="flex items-center justify-between p-3">
         <button
           onClick={() => setMenuOpen(!menuOpen)}
           className="md:hidden p-2 text-white"
@@ -18,10 +18,19 @@ const Header = () => {
           {menuOpen ? <X size={50} /> : <Menu size={50} />}
         </button>
 
+        <nav className={`  absolute left-2 top-48 w-50  z-50 ${menuOpen ? 'block' : 'hidden'} md:static md:flex md:w-auto`}>
+          <ul className='flex flex-col gap-[3rem] md:flex-row md:space-x-12 lg:space-x-16 text-5xl p-3 md:text-2xl lg:text-3xl text-white p-4 md:p-0'>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/produtos">Produtos</Link></li>
+            <li><Link to="/sobre">Sobre</Link></li>
+            <li><Link to="/contato">Contato</Link></li>
+          </ul>
+        </nav>
+
         <img 
           src='./src/assets/logo.png' 
           alt='Logo da empresa' 
-          className='w-[12rem] md:w-[11rem] lg:w-[12rem] xl:w-[15rem] object-contain'
+          className='w-[17rem] mt-5 md:w-[11rem] lg:w-[12rem] xl:w-[15rem] object-contain'
         />
         <ShoppingCart 
           size={50} 
@@ -29,14 +38,7 @@ const Header = () => {
         />
       </div>
 
-      <nav className={`  absolute left-2 top-48 w-50  z-50 ${menuOpen ? 'block' : 'hidden'} md:static md:flex md:w-auto`}>
-        <ul className='flex flex-col gap-[3rem] md:flex-row md:space-x-12 lg:space-x-16 text-5xl p-3 md:text-2xl lg:text-3xl text-white p-4 md:p-0'>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/produtos">Produtos</Link></li>
-          <li><Link to="/sobre">Sobre</Link></li>
-          <li><Link to="/contato">Contato</Link></li>
-        </ul>
-      </nav>
+
     </header>
 
 )}
--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,11 +1,35 @@
 const Home = () => {
     return (
-        <>
-        <div className="flex m-10">
-            <img src="./src/assets/banner.png" alt="Promoção Gta 6" className="w-10% h-100%"/>
-        </div>
-        </>
+        <main>
+            <section>
+                <h1 className="flex justify-center mt-10 text-5xl">Nossas Ofertas</h1>
+                <div className="bg-gray-200 m-5 p-1 rounded-lg shadow-md flex flex-col items-center">
+                    <div className="flex m-3">
+                        <img src="./src/assets/banner.png" alt="Promoção Gta 6" className="box-border shadow-lg"/>
+                    </div>
+                    <button 
+                    // onClick {() => {
+                    //     const element = document.getElementById('gta6-section');
+                    //     if (element) {
+                    //         element.scrollIntoView({ behavior: 'smooth' });
+                    //     }
+                    //     }}
+                    className="bg-purple-900 hover:scale-[1.02] transition-transform  flex items-center justify-center rounded-lg m-3 shadow-lg "
+                    > 
+                        <p className="text-3xl text-gray-200 m-7 ">
+                            Compre Aqui
+                        </p>
+                    </button>
+                </div>
+            </section>
 
+            <section>
+                <h1 className="flex justify-center mt-10 text-5xl">Produtos</h1>
+
+
+            </section>
+
+        </main>
     )
 }
 
